13f838a5a629f9aab7a6e91e47f18c099b744d5e
Om Duggineni <omduggineni@gmail.com>
2022-05-03 23:19:27 +0000

    v1.0.1 - fixed pip packaging

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name='termvideo',
-    version='1.0',
+    version='1.0.1',
     scripts=['termvideo.py'],
+    author='Om Duggineni',
+    install_requires=['numpy', 'opencv-python'],
     license="LGPLv3",
     entry_points = {
         'console_scripts': ['termvideo=termvideo:main'],
