d3ad51d1ccfad8dc2f39960cbf3c12a53bb70068
Om Duggineni <omduggineni@gmail.com>
2022-05-04 01:02:51 +0000

    refactored code and added delay for fps

--- termvideo.py
+++ termvideo.py
@@ -4,8 +4,22 @@ import cv2
 import sys
 import shutil
 import numpy as np
+import argparse
+import time
 
-def display_frame(frame):
+last_frame_timestamp = None
+
+def display_frame(frame, fps=30):
+    global last_frame_timestamp
+
+    #delay based on fps using time
+    if last_frame_timestamp is not None:
+        delay = int(1e9/fps) - (time.time_ns() - last_frame_timestamp)
+        #print(delay)
+        if delay > 0:
+            time.sleep(delay/1e9)
+    last_frame_timestamp = time.time_ns()
+
     #get terminal size
     columns, rows = shutil.get_terminal_size()
     #resize frame to fit terminal
@@ -25,14 +39,29 @@ def display_frame(frame):
             #print to terminal
             sys.stdout.write(" ")
 
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Display video')
+    parser.add_argument('video_name', type=str, help='video name')
+    return parser.parse_args()
+
 def main():
-    video_name = sys.argv[1]
+    args = get_args()
+    video_name = args.video_name
     cap = cv2.VideoCapture(video_name)
-    # image = cv2.imread("ppmSample.ppm")
-    # display_frame(image)
+    if not cap.isOpened():
+        print("Error opening video stream or file")
+        sys.exit(1)
+    
+    #get frame rate
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
-            display_frame(frame)
+            display_frame(frame, fps)
         else:
             break
+
+if __name__ == '__main__':
+    main()
